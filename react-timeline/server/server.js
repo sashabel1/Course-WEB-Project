c3f809b50191a14df1ded55606cdf5987785dbb0
sashabel1 <alexa2034@gmail.com>
2025-06-21 12:23:56 +0300

    fix to 1 env file

--- react-timeline/server/server.js
+++ react-timeline/server/server.js
@@ -1,5 +1,4 @@
-
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
