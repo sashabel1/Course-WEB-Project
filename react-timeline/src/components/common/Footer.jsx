d763e89b9554602713d685fc4132c4d45a327945
sashabel1 <alexa2034@gmail.com>
2025-06-26 18:18:17 +0300

    header and fotter components

--- react-timeline/src/components/common/Footer.jsx
+++ react-timeline/src/components/common/Footer.jsx
@@ -1,11 +1,11 @@
-import '../../style/componentsStyle/Footer.css';
-
 function Footer() {
   return (
-    <footer className="footer text-center text-sm">
+    <footer
+      className="w-full bg-[#004f53] text-[#F2EFE7] py-4 text-center text-base font-normal mt-0 select-none shadow-inner">
       &copy; 2025 Timeline History Website. All rights reserved.
     </footer>
   );
 }
 
 export default Footer;
+
